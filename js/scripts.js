70058dee5218bc05e9ce0f877d21aae8947a2fa7
Austin Schrader <austin.jschrader@gmail.com>
2019-02-03 13:12:36 -0800

    Tell users if their stress warnings are more than their stress reduction methods.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+
+    $("#warning-responses").show();
+    var warningModes = $("input:checkbox[name=stress-warning]:checked");
+    warningModes.each(function(){
+      var stressWarningMode = $(this).val();
+      $('#warning-responses').append(stressWarningMode + "<br>");
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
+
+    $("#health-responses").show();
+    var healthModes = $("input:checkbox[name=stress-health]:checked")
+    healthModes.each(function(){
+      var stressHealthMode = $(this).val();
+      $('#health-responses').append(stressHealthMode + "<br>");
     });
+
+    $("#stress-responses").show();
+    var reduceModes = $("input:checkbox[name=stress-reduce]:checked")
+    reduceModes.each(function() {
+      var stressReduceMode = $(this).val();
+      $('#stress-responses').append(stressReduceMode + "<br>");
+    });
+
     $('#transportation_survey').hide();
+
+    if(warningModes.length > reduceModes.length) {
+      alert("You are stressed and not doing anything about it!");
+    } else {
+      alert("Good");
+    }
+
   });
 });
